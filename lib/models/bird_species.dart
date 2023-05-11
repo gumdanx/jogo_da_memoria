6d0e36c7cbbc844e79d29983b7ef3d4152ec9776
Tiago Peres Moura Fe <tpmf@ic.ufal.br>
2023-05-11 02:41:46 -0300

    Ajuste na identação

--- lib/models/bird_species.dart
+++ lib/models/bird_species.dart
@@ -3,7 +3,9 @@ class BirdSpecies {
   String image;
   int numCorrect;
 
-  BirdSpecies({required this.name,
+  BirdSpecies({
+    required this.name,
     required this.image,
-    required this.numCorrect});
+    required this.numCorrect
+  });
 }
